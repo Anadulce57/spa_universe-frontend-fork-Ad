e65df0cda5e077846dd9ba94482d9cfc30c75a08
Anadulce57 Martins <andl2008@hotmail.com>
2024-12-04 13:31:36 +0000

    Começo da API base

---
+++ Back-end/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require("cors")
+const mysql = require("mysql2")
+const jwt = require("jsonwebtoken")
+
+const app = express() //recebe o express
+
+app.use(cors()) //front-end acesse esse espaço
+app.use(express.json()) //Converter informações em objeto javascript
+
+app.post("/register", (request, response) =>{
+    const user = request.body.user
+
+    console.log(user)
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})

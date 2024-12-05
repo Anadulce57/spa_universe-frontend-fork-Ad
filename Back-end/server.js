473f4c744505ba7e9530012d8f8962dc345a4cea
Anadulce57 Martins <andl2008@hotmail.com>
2024-12-05 13:32:25 +0000

    Banco de dados sigiloso

--- Back-end/server.js
+++ Back-end/server.js
@@ -5,6 +5,8 @@ const jwt = require("jsonwebtoken")
 
 const app = express() //recebe o express
 
+const {DB_HOST, DB_NAME, DB_USER, DB_PASSWORD} = process.env
+
 app.use(cors()) //front-end acesse esse espaço
 app.use(express.json()) //Converter informações em objeto javascript
 
@@ -24,8 +26,8 @@ app.listen(3000, () => {
 
 const db = mysql.createPool({
     connectionLimit: 10,
-    host: "sql10.freesqldatabase.com",
-    datbase: "sql10750023",
-    user: "sql10750023",
-    password: "y7bvnYfAa2"
+    host: DB_HOST,
+    datbase: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD
 })
